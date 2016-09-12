acc16864754053d971c9bae218b7f4b02a9d0b6b
Manuel Lagang <manuellagang@gmail.com>
2016-09-11 20:14:04 -0700

    ex6: Make a CoordTreeMap class
    Makes for a cleaner abstraction. Seems to be much faster as well

--- app/ex6/ex6.hs
+++ app/ex6/ex6.hs
@@ -43,8 +43,8 @@ commandParser = do
 
 rangesIntersect :: CoordRange -> CoordRange -> Bool
 rangesIntersect (CoordRange (minX1, minY1) (maxX1, maxY1)) (CoordRange (minX2, minY2) (maxX2, maxY2)) =
-  (((minX1 <= minX2) && (minX2 <= maxX1)) || ((minX2 <= minX1) && (maxX1 <= maxX2))) &&
-  (((minY1 <= minY2) && (minY2 <= maxY1)) || ((minY2 <= minY1) && (maxY1 <= maxY2)))
+  (((minX1 <= minX2) && (minX2 <= maxX1)) || ((minX2 <= minX1) && (minX1 <= maxX2))) &&
+  (((minY1 <= minY2) && (minY2 <= maxY1)) || ((minY2 <= minY1) && (minY1 <= maxY2)))
 
 singleDimRanges :: Integer -> Integer -> Integer -> Integer -> [((Integer, Integer), Bool)]
 singleDimRanges minTarget minQuery maxQuery maxTarget
@@ -81,50 +81,73 @@ rangeQuery (CoordRange (minTargetX, minTargetY) (maxTargetX, maxTargetY)) (Coord
       ((y1, y2), inQueryY) <- yRanges
       return ((CoordRange (x1, y1) (x2, y2)), inQueryX && inQueryY)
 
+data CoordRangeTree a = Leaf CoordRange a | Node CoordRange [(CoordRangeTree a)]
+
+coordRangeUpdate :: CoordRangeTree a -> CoordRange -> (a -> a) -> CoordRangeTree a
+coordRangeUpdate (Leaf targetCoordRange val) queryCoordRange f =
+  let subRanges = rangeQuery targetCoordRange queryCoordRange in
+    if length subRanges == 1 then
+      let (range, intersect) = head subRanges in
+        if intersect then
+          Leaf targetCoordRange (f val)
+        else
+          Leaf targetCoordRange val
+    else
+      Node targetCoordRange
+       (do
+          (subRange, intersects) <- subRanges
+          if intersects then
+            return $ Leaf subRange (f val)
+          else
+            return $ Leaf subRange val)
+
+coordRangeUpdate (Node targetCoordRange children) queryCoordRange f =
+  if rangesIntersect targetCoordRange queryCoordRange then
+    Node targetCoordRange (map (\child -> coordRangeUpdate child queryCoordRange f) children)
+  else
+    Node targetCoordRange children
+
+coordRangeTreeFoldMap :: (CoordRange -> a -> b) -> ([b] -> b) -> CoordRangeTree a -> b
+coordRangeTreeFoldMap f combine (Leaf coordRange val) = f coordRange val
+coordRangeTreeFoldMap f combine (Node coordRange children) = combine (map (coordRangeTreeFoldMap f combine)  children)
+
 rangeSize :: CoordRange -> Integer
 rangeSize (CoordRange (minX, minY) (maxX, maxY)) = ((maxX - minX + 1) * (maxY - minY + 1))
 
-subRangeLightsOn :: CoordRange -> CommandType -> [Command] -> Integer
-subRangeLightsOn coordRange commandType restCommands =
-  let s = rangeSize coordRange in
-    case commandType of
-      TurnOff -> 0
-      TurnOn  -> s
-      Toggle  -> s - (numLightsOnRev restCommands coordRange)
+lightsOnUpdate :: CoordRangeTree Bool -> Command -> CoordRangeTree Bool
+lightsOnUpdate tree (Command commandType coordRange) =
+  let updateFunc = case commandType of
+        TurnOff -> const False
+        TurnOn -> const True
+        Toggle -> not in
+    coordRangeUpdate tree coordRange updateFunc
 
-numLightsOnRev :: [Command] -> CoordRange -> Integer
-numLightsOnRev [] range = 0
-numLightsOnRev ((Command commandType commandRange) : restCommands) range =
-  let subRanges = rangeQuery range commandRange in
-    sum $ do
-      (subRange, intersects) <- subRanges
-      if intersects then
-        return $ subRangeLightsOn subRange commandType restCommands
-      else
-        return $ numLightsOnRev restCommands subRange
+numLightsOnInRange :: CoordRange -> Bool -> Integer
+numLightsOnInRange _ False = 0
+numLightsOnInRange coordRange True = rangeSize coordRange
 
-numLightsOn commands coordRange = numLightsOnRev (reverse commands) coordRange
+numLightsOn :: [Command] -> CoordRange -> Integer
+numLightsOn commands coordRange =
+  let startState = Leaf coordRange False
+      endState = foldl lightsOnUpdate startState commands in
+    coordRangeTreeFoldMap numLightsOnInRange sum endState
 
-subRangeBrightness :: CoordRange -> CommandType -> [Command] -> Integer -> Integer
-subRangeBrightness coordRange commandType restCommands brightnessPerLight =
-  let newBrightnessPerLight = case commandType of
-        TurnOff -> max (brightnessPerLight - 1) 0
-        TurnOn  -> brightnessPerLight + 1
-        Toggle  -> brightnessPerLight + 2 in
-    brightnessAdd restCommands coordRange newBrightnessPerLight
+brightnessUpdate :: CoordRangeTree Integer -> Command -> CoordRangeTree Integer
+brightnessUpdate tree (Command commandType coordRange) =
+  let updateFunc = case commandType of
+        TurnOff -> (\n -> max 0 (n - 1))
+        TurnOn -> (+ 1)
+        Toggle -> (+ 2) in
+    coordRangeUpdate tree coordRange updateFunc
 
-brightnessAdd :: [Command] -> CoordRange -> Integer -> Integer
-brightnessAdd [] range brightnessPerLight = brightnessPerLight * (rangeSize range)
-brightnessAdd ((Command commandType commandRange) : restCommands) range brightnessPerLight =
-  let subRanges = rangeQuery range commandRange in
-    sum $ do
-      (subRange, intersects) <- subRanges
-      if intersects then
-        return $ subRangeBrightness subRange commandType restCommands brightnessPerLight
-      else
-        return $ brightnessAdd restCommands subRange brightnessPerLight
+brightnessInRange :: CoordRange -> Integer -> Integer
+brightnessInRange coordRange brightness = brightness * (rangeSize coordRange)
 
-brightness commands coordRange = brightnessAdd commands coordRange 0
+brightness :: [Command] -> CoordRange -> Integer
+brightness commands coordRange = 
+  let startState = Leaf coordRange 0
+      endState = foldl brightnessUpdate startState commands in
+    coordRangeTreeFoldMap brightnessInRange sum endState
 
 parseWithError :: Parser a -> String -> a
 parseWithError p a = case (parse p "" a) of
